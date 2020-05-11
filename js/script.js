7405e315e483379baea2b0299f0a902942b867b2
DanielRY <danielrojasy@upeu.edu.pe>
2020-05-11 16:16:51 -0500

    edicion 1

--- js/script.js
+++ js/script.js
@@ -11,21 +11,25 @@ switch (parseFloat(opcion)) {
         document.write("resta: ",x-y);
         break;
     case 3:
-        x=prompt("N1: ")
-        y=prompt("N2: ")
+        x=parseFloat(prompt("N1: "))
+        y=parseFloat(prompt("N2: "))
         document.write("multiplicacion: ",x*y);
         break;
     case 4:
-        x=prompt("N1: ")
-        y=prompt("N2: ")
-        document.write("division: ",x/y);
+        x=parseFloat(prompt("N1: "))
+        y=parseFloat(prompt("N2: "))
+        if (y>0) {
+            document.write("division: ",x/y);   
+        } else {
+            document.write("Indefinido")
+        }
         break;
     case 5:
-        x=prompt("N1: ")
+        x=parseFloat(prompt("N1: "))
         document.write("Raiz cubica: ",Math.pow(x,1/3));
         break;
     case 6:
-        x=prompt("N1: ")
+        x=parseFloat(prompt("N1: "))
         var total=1
         for (i=1; i<=x; i++) {
             total = total * i; 
@@ -33,10 +37,10 @@ switch (parseFloat(opcion)) {
         document.write("factorial: ",total)
         break;
     case 7:
-        x=prompt("Base: ")
-        y=prompt("Exponente: ")
+        x=parseFloat(prompt("base: "))
+        y=parseFloat(prompt("exponente: "))
         document.write("potencia: ",Math.pow(x,y));
         break;
-    default:
+    default:document.write("x_X ERROR x_x")
         break;
 }
